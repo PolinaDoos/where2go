d79033e8222a6f460e095afb779df970ed21e959
Polina Doos <87525081+PolinaDoos@users.noreply.github.com>
2022-05-17 18:17:43 +0300

    logic improved

--- webapp/country/views.py
+++ webapp/country/views.py
@@ -1,3 +1,4 @@
+import logging
 from flask import render_template, redirect, flash, url_for, request, Blueprint
 from flask_login import current_user, login_required
 
@@ -9,90 +10,91 @@ from webapp.countries_rosturizm import get_countries_rosturizm, filter_set_of_he
 from webapp import log
 
 
-blueprint = Blueprint('country_related', __name__, url_prefix='/countries')
+blueprint = Blueprint("country_related", __name__, url_prefix="/countries")
 
 
-@blueprint.route('/process_country', methods=['GET', 'POST'])
+@blueprint.route("/process_country", methods=["GET", "POST"])
 def check_signin():
     if current_user.is_authenticated:
         return process_country()
     else:
-        flash('пожалуйста, авторизируйтесь')
-        return redirect(url_for('user_related.login'))
+        flash("пожалуйста, авторизируйтесь")
+        return redirect(url_for("user_related.login"))
 
 
 def process_country():
     form = CounryChoose()
     if form.validate_on_submit():
-        select_dep = request.form.get('country_dep')
-        select_arr = request.form.get('country_arr')
+        select_dep = request.form.get("country_dep")
+        select_arr = request.form.get("country_arr")
     if select_dep != select_arr:
         choice = UserRequest(user_id=current_user.id, country_dep=select_dep, country_arr=select_arr)
         db.session.add(choice)
         db.session.commit()
         log.logging.info(choice)
-        return redirect(url_for('country_related.country_request'))
-    flash('Вы указали одинаковые страны, попробуйте еще')
-    return redirect(url_for('main_page.display'))
+        return redirect(url_for("country_related.country_request"))
+    flash("Вы указали одинаковые страны, попробуйте еще")
+    return redirect(url_for("main_page.display"))
 
 
-@blueprint.route('/process_country_from_list')
+@blueprint.route("/process_country_from_list")
 def process_country_from_list():
-    id = int(request.args.get('identifier'))
+    id = int(request.args.get("identifier"))
     open_countries = get_open_countries()
     for element in open_countries:
-        if element['country_id'] == id:
-            select_arr = element['country_name']
+        if element["country_id"] == id:
+            select_arr = element["country_name"]
             break
-    select_dep = 'Россия'
+    select_dep = "Россия"
     choice = UserRequest(user_id=current_user.id, country_dep=select_dep, country_arr=select_arr)
     db.session.add(choice)
     db.session.commit()
-    return redirect(url_for('country_related.country_request', identifier=id))
+    return redirect(url_for("country_related.country_request", identifier=id))
 
 
-@blueprint.route('/country_request')
+@blueprint.route("/country_request")
 @login_required
 def country_request():
-    title = f'Актуальная информация по странам'
-    que = UserRequest.query.filter(UserRequest.user_id==current_user.id).order_by(UserRequest.id.desc()).limit(1)[0]
+    title = f"Актуальная информация по странам"
+    que = UserRequest.query.filter(UserRequest.user_id == current_user.id).order_by(UserRequest.id.desc()).limit(1)[0]
     dep = que.country_dep
     arr = que.country_arr
     restrictions_by_country = country_conditions_request(arr)
     covid_data = country_covid_request(arr)
-    return render_template('country/country_request.html', page_title=title, 
-                                country_dep=dep, country_arr=arr,
-                                restrictions_by_country=restrictions_by_country,
-                                covid_data=covid_data)
+    return render_template(
+        "country/country_request.html",
+        page_title=title,
+        country_dep=dep,
+        country_arr=arr,
+        restrictions_by_country=restrictions_by_country,
+        covid_data=covid_data,
+    )
 
 
 def country_conditions_request(arr):
-    '''Возвращает кортеж из 1 элемента при ошибке подключения,
+    """Возвращает кортеж из 1 элемента при ошибке подключения,
     возвращает None при отсутствии страны на сайте Ростуризма,
     возвращает кортеж из 6 элементов, если получен ожидаемый набор данных,
-    возвращает кортеж из 7 элементов, если неожидаемый набор'''
+    возвращает кортеж из 7 элементов, если неожидаемый набор"""
 
-    log.logging.info(arr)
-    restrictions_by_country = get_tuple_info_rosturizm(arr)
     no_data_by_field = "Ошибка подключения. Обновите страницу"
-    if restrictions_by_country is None:
-        return no_data_by_field,
-    elif restrictions_by_country == {}:
+    restrictions_by_country_dirty = get_tuple_info_rosturizm(arr)
+    if restrictions_by_country_dirty is None:
+        return (no_data_by_field,)
+    elif restrictions_by_country_dirty == {}:
         return None
-    else:
-        restrictions_by_country = get_tuple_info_rosturizm(arr)[0]
-        unusual_output_rosturizm = get_tuple_info_rosturizm(arr)[1]
-        transportation = restrictions_by_country.get('transportation')
-        visa = restrictions_by_country.get('visa')
-        vaccine = restrictions_by_country.get('vaccine')
-        conditions = restrictions_by_country.get('conditions')
-        open_objects = restrictions_by_country.get('open_objects')
-        restrictions = restrictions_by_country.get('restrictions')
-        log.logging.info(restrictions_by_country)
-        if not unusual_output_rosturizm:
-            return transportation, visa, vaccine, conditions, open_objects, restrictions
-        else:
-            return transportation, visa, vaccine, conditions, open_objects, restrictions, 'unusual'
+    restrictions_by_country = restrictions_by_country_dirty[0]
+    log.logging.info(restrictions_by_country)
+    unusual_output_rosturizm = restrictions_by_country_dirty[1]
+    transportation = restrictions_by_country.get("transportation")
+    visa = restrictions_by_country.get("visa")
+    vaccine = restrictions_by_country.get("vaccine")
+    conditions = restrictions_by_country.get("conditions")
+    open_objects = restrictions_by_country.get("open_objects")
+    restrictions = restrictions_by_country.get("restrictions")
+    if not unusual_output_rosturizm:
+        return transportation, visa, vaccine, conditions, open_objects, restrictions
+    return transportation, visa, vaccine, conditions, open_objects, restrictions, "unusual"
 
 
 def get_open_countries(countries_list=get_countries_rosturizm()):
@@ -101,21 +103,25 @@ def get_open_countries(countries_list=get_countries_rosturizm()):
         country_from_db = Country.query.filter_by(country_name=country).first()
         countries_data = {}
         if country_from_db:
-            countries_data['country_id'] = country_from_db.id
-            countries_data['country_name'] = country_from_db.country_name
+            countries_data["country_id"] = country_from_db.id
+            countries_data["country_name"] = country_from_db.country_name
             country_to_id_mapping.append(countries_data)
     log.logging.info(country_to_id_mapping)
     return country_to_id_mapping
 
 
-@blueprint.route('/country_list')
+@blueprint.route("/country_list")
 def display_countries_list():
     title = f"Какие страны открыты для россиян"
     countries_list = get_countries_rosturizm()
     country_to_id_mapping = get_open_countries(countries_list)
 
-    return render_template('country/country_list.html', page_title=title, countries_list=countries_list,
-                            country_to_id_mapping=country_to_id_mapping)
+    return render_template(
+        "country/country_list.html",
+        page_title=title,
+        countries_list=countries_list,
+        country_to_id_mapping=country_to_id_mapping,
+    )
 
 
 # возвращает словарь из ответов по covid для страны
